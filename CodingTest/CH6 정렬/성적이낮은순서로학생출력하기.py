307897291572e57ff4d62f6520fd9915e2210b40
heerucan <iwpow0307@gmail.com>
2022-02-17 23:46:44 +0900

    [코딩테스트책] 6-2. 성적이 낮은 순서로 학생 출력하기 (#4)

---
+++ CodingTest/CH6 정렬/성적이낮은순서로학생출력하기.py
@@ -0,0 +1,14 @@
+# 2
+# 홍길동 95
+# 이순신 77
+
+N = int(input())
+student = []
+result = []
+for i in range(N):
+    student.append(list((input().split())))
+
+sortedStudent = sorted(dict(student).items())
+
+for i in range(len(sortedStudent)):
+    print(sortedStudent[i][0], end= ' ')
