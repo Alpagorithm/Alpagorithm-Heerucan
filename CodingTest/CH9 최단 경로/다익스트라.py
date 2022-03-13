a80f4a19d69f6fbe8c6b1b55075019c7d0c4fe5a
heerucan <iwpow0307@gmail.com>
2022-03-13 19:15:02 +0900

    [Chore] 코드 수정 (#7)

--- CodingTest/CH9 최단 경로/다익스트라.py
+++ CodingTest/CH9 최단 경로/다익스트라.py
@@ -36,7 +36,7 @@ def dijkstra(start):
             continue
         # 현재 노드와 연결된 다른 인접한 노드들을 확인
         for i in graph[now]:
-            cost = dist + [1]
+            cost = dist + i[1]
             # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
@@ -47,7 +47,7 @@ dijkstra(start)
 # 모든 노드로 가기 위한 최단 거리를 출력
 for i in range(1, n+1):
     # 도달할 수 없는 경우, 무한이라고 출력
-    if distance[i] == INF
+    if distance[i] == INF:
         print("무한")
     else:
         print(distance[i])
