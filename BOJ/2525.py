60febb541826d4897d27883739d5ef1903c2b743
heerucan <iwpow0307@gmail.com>
2022-04-12 16:28:52 +0900

    [BOJ] 2525

---
+++ BOJ/2525.py
@@ -0,0 +1,22 @@
+a, b = map(int, input().split())
+c = int(input())
+
+if (b+c) >= 60:
+    if (b+c) % 60 >= 0:
+        a += (b+c)//60
+        b = (b+c)-(60*((b+c)//60))
+        if a >= 24:
+            # a = 0
+            a = a - 24
+    else:
+        a += 1
+        b = (b+c - 60)
+        if a >= 24:
+            a = a - 24
+            # a = 24 - a
+    print(a, b)
+else:
+    print(a, b+c)
+
+
+
