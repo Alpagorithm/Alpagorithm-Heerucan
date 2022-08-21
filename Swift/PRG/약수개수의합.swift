60183a15dd389354819b21d4840952053f8bc5ea
heerucan <iwpow0307@gmail.com>
2022-08-21 18:54:07 +0900

    PRG - 약수의 합 : 코드 개선

--- Swift/PRG/약수개수의합.swift
+++ Swift/PRG/약수개수의합.swift
@@ -7,12 +7,27 @@
 
 import Foundation
 
-func solution(_ n:Int) -> Int {
-    var array: [Int] = []
-    for i in 1...n {
-        if n % i == 0 {
-            array.append(i)
+func solution2(_ n:Int) -> Int {
+    if n != 0 {
+        var array: [Int] = []
+        for i in 1...n {
+            if n % i == 0 {
+                array.append(i)
+            }
         }
+        return array.reduce(0, +)
+    } else {
+        return 0
     }
-    return array.reduce(0, +)
 }
+
+// 개선한 코드
+
+func solution(_ n:Int) -> Int {
+    if n == 0 {
+        return 0
+    } else {
+        return Array(1...n).filter { n % $0 == 0 }.reduce(.zero, +)
+    }
+}
+solution(12)
