f350ac4eecb5efe6e9ed0aed12a33fd027f954d4
heerucan <iwpow0307@gmail.com>
2022-10-02 21:49:06 +0900

    PRG - 서울에서 김서방찾기

---
+++ Swift/PRG/서울에서김서방찾기.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  서울에서김서방찾기- level1
+//
+//  Created by heerucan on 2022/08/14.
+//
+
+import Foundation
+
+func solution(_ seoul:[String]) -> String {
+        return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
+}
